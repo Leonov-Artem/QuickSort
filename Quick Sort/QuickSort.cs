7702c1dde579359854ee360d1c85aa39a76f5cda
Leonov Artem <leonovru@yandex.ru>
2019-01-28 16:19:52 +0300

    Add method 'InsertionSort'

--- Quick Sort/QuickSort.cs
+++ Quick Sort/QuickSort.cs
@@ -10,7 +10,6 @@ namespace Quick_Sort
     {
         /*
              * InsertionSort()
-             * Swap()
              * RecursiveQuickSort()
              * Partition()
         */
@@ -18,6 +17,21 @@ namespace Quick_Sort
 
         public QuickSort(T[] array) => this.array = array;
 
+        private void InsertionSort(int left, int right)
+        {
+            for (int i = left + 1; i <= right; i++)
+            {
+                T temp = array[i];
+                int inner = i;
+
+                while (inner > left && array[inner-1].CompareTo(temp) >= 0)
+                {
+                    array[inner] = array[inner - 1];
+                    inner--;
+                }
+                array[inner] = temp;
+            }
+        }
         private void Swap(int first, int second)
         {
             T temp = array[first];
